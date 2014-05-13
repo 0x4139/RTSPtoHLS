484564a6a088f3338cbb9a64a94d38b214682a0d
Vali Malinoiu <0x4139@gmail.com>
2014-05-13 22:41:05 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var fork = require("child_process")
+var app = express();
+var pump = require('pump');
+
+var address= 'rtsp://admin:admin@83.166.215.254:554/cam/realmonitor?channel=';
+
+app.get('/cam/:index', function(req, res){
+
+  res.writeHead(200,{'Content-Type':'video/mp4'});
+  var cameraStream = fork.spawn("./ffmpeg", [
+                        "-i",address+req.params.index,
+                        "-codec:v", "copy",
+                        "-bufsize","1000k",
+                        "-threads","1",
+                        "-codec:a","libfdk_aac",
+                        "-f","mp4",
+                        "-movflags","frag_keyframe",
+                        "-"
+                        ],  {detached: false});
+  pump(cameraStream.stdout,res);
+  res.on("close",function(){
+      cameraStream.kill();
+  });
+
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
